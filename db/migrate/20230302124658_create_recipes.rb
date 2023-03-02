423ae69a2e8a09f25aa979e7e932eed3a2ed82b7
asiamohammed <mohamedasia021@gmail.com>
2023-03-02 15:56:18 +0300

    commit

--- db/migrate/20230302124658_create_recipes.rb
+++ db/migrate/20230302124658_create_recipes.rb
@@ -1,7 +1,6 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
-      t.integer :id
       t.string :type
       t.string :name
       t.string :image
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_072816) do
+ActiveRecord::Schema.define(version: 2023_03_02_124658) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "type"
+    t.string "name"
+    t.string "image"
+    t.string "ingredients"
+    t.string "instructions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
